a6cbcd46c80eb27afa00db730b53742aa60d1fe6
Steven <Steven@10.0.1.7>
2013-01-14 17:03:35 -0500

    Created Netbeans project.

---
+++ Pirate-Ship/src/pirate/ship/PirateShip.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pirate.ship;
+
+/**
+ *
+ * @author Steven
+ */
+public class PirateShip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	// TODO code application logic here
+    }
+}
