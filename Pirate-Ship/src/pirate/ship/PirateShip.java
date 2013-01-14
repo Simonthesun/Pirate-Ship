581d36b517a21b946c38a950f43aa69317085d95
Steven <Steven@10.0.1.7>
2013-01-14 17:31:52 -0500

    Created a Vessel object.

--- Pirate-Ship/src/pirate/ship/PirateShip.java
+++ Pirate-Ship/src/pirate/ship/PirateShip.java
@@ -1,19 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pirate.ship;
+
+import vessel.*;
 
 /**
  *
  * @author Steven
  */
 public class PirateShip {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-	// TODO code application logic here
+	Vessel boat = new Vessel("Wench Mobile", 169, 4, 101);
+	
     }
 }
