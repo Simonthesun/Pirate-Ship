32a9853dcb83ae51bab1265449e9f2ddb7e63ff8
Steven <Steven@10.0.1.8>
2013-01-14 21:01:58 -0500

    Fixed the part of speech tag ((A), (N), (I)). The while loop wasn't triggering before, I changed the logic. I also added code I stole from stackexchange to remove parenthesis from the final product. "\\([^]*"..."\\)"

--- Pirate-Ship/src/pirate/seamen/Sailor.java
+++ Pirate-Ship/src/pirate/seamen/Sailor.java
@@ -20,13 +20,13 @@ public class Sailor {
     
     public String talkLikeAPirate(){
 	String sentence;
-	if(Math.random() > 0.5){
+	if(Math.random() > 0.9){
 	    sentence = Vernacularrrgh.PICK_UP_LINES[(int)(Math.random() * Vernacularrrgh.PICK_UP_LINES.length)];
 	} else {
 	    sentence = Vernacularrrgh.SENTENCES[(int)(Math.random() * Vernacularrrgh.SENTENCES.length)];
 	    for(int i = 0; i < Vernacularrrgh.PARTS_OF_SPEECH.length; i++){
-		while(sentence.indexOf(name) > -1){
-		    sentence = sentence.replaceFirst(Vernacularrrgh.PARTS_OF_SPEECH[i], Vernacularrrgh.get(Vernacularrrgh.PARTS_OF_SPEECH[i]));
+		while(sentence.contains(Vernacularrrgh.PARTS_OF_SPEECH[i])) {//sentence.indexOf(name) > -1){
+		    sentence = sentence.replaceFirst("\\([^)]*" + Vernacularrrgh.PARTS_OF_SPEECH[i] + "\\)", Vernacularrrgh.get(Vernacularrrgh.PARTS_OF_SPEECH[i]));
 		}
 	    }
 	}
