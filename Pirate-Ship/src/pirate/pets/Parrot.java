20f9535097b299c77b616a6d630ee336c0503ac5
Simon Sun <simonthesun@yahoo.com>
2013-01-18 13:27:07 -0500

    Added pirate.pets package and Parrot class.

---
+++ Pirate-Ship/src/pirate/pets/Parrot.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pirate.pets;
+
+/**
+ *
+ * @author simonthesun
+ */
+public class Parrot {
+    
+    String name;
+    String owner;
+    public boolean canTalk = true;
+    
+    public Parrot (String name, String owner, boolean talk) {
+        this.name = name + " the Parrot";
+        this.owner =  owner + "'s parrot";
+        talk = canTalk;
+        
+    }
+}
