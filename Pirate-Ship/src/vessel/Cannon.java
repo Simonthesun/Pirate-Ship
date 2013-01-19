34a133860d9deff75067f9c50f0d883f8b4b7c41
Simon Sun <simonthesun@yahoo.com>
2013-01-19 14:01:42 -0500

    Added Cannon class.

---
+++ Pirate-Ship/src/vessel/Cannon.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vessel;
+
+/**
+ *
+ * @author Simon
+ */
+public class Cannon {
+    
+    private int _diameter;
+    
+    public Cannon (int diameter) {
+        _diameter = diameter;
+    }
+}
