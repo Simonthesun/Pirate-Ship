109e2d2090c99ecedb020a87076e42edbe6069fb
Simon Sun <simonthesun@yahoo.com>
2013-01-19 10:35:14 -0500

    Added squawk() method.

--- Pirate-Ship/src/pirate/pets/Parrot.java
+++ Pirate-Ship/src/pirate/pets/Parrot.java
@@ -1,13 +1,14 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
- * hi
  */
 package pirate.pets;
 
+import pirate.seamen.Vernacularrrgh;
+
 /**
  *
- * @author simonthesun
+ * @author Simon
  */
 public class Parrot {
     
@@ -19,6 +20,31 @@ public class Parrot {
         this.name = name + " the Parrot";
         this.owner =  owner + "'s parrot";
         talk = canTalk;
+    }
+    
+    public String talk() {
+        String noise;
         
+        if (canTalk = true) {
+            if (Math.random() < 0.7) {
+                noise = "Polly want a cracker!";
+            } else {
+                noise = Vernacularrrgh.SENTENCES[(int)(Math.random() * Vernacularrrgh.SENTENCES.length)];
+                for(int i = 0; i < Vernacularrrgh.PARTS_OF_SPEECH.length; i++){
+                    while(noise.contains(Vernacularrrgh.PARTS_OF_SPEECH[i])) {//sentence.indexOf(name) > -1){
+                        noise = noise.replaceFirst("\\([^)]*" + Vernacularrrgh.PARTS_OF_SPEECH[i] + "\\)", Vernacularrrgh.get(Vernacularrrgh.PARTS_OF_SPEECH[i]));
+                    }
+                }
+            }
+            return "Squawk!" + noise;
+        } else {
+            return null;
+        }
+    }
+    
+    public String squawk() {
+        String squawk = "Squawk!";
+        
+        return squawk;
     }
 }
